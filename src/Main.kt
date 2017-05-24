45cce32cf55884cb0977d519df85a5ffd3959c95
Luis Marzoratti <lmarzora@itba.edu.ar>
2017-05-24 15:59:40 -0300

    gear id

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,10 @@
+import characters.Warrior
 import gear.Gear
 
 fun main(args: Array<String>) {
 
-	val plateArmor = Gear(5.0, 5.0, 5.0, 5.0, 50.0)
-	val sword = Gear(20.0, 1.0, 1.0, 1.0, 0.0)
+	val plateArmor = Gear(1, 5.0, 5.0, 5.0, 5.0, 50.0)
+	val sword = Gear(2, 20.0, 1.0, 1.0, 1.0, 0.0)
 	var warrior = Warrior(1.5, mutableMapOf("armor" to plateArmor, "weapon" to sword))
 
 	print("Amazing Fitness of ${warrior.getFitness()}")
--- src/gear/Gear.kt
+++ src/gear/Gear.kt
@@ -1,7 +1,13 @@
 package gear
 
-data class Gear(val strength: Double,
+data class Gear(val id: Int,
+                val strength: Double,
                 val dexterity: Double,
                 val expertise: Double,
                 val resistance: Double,
                 val life: Double)
+
+fun parseGearFile(line: String): Gear {
+    var stats = line.split(',').map(String::toDouble)
+    return Gear(stats[0].toInt() ,stats[1],stats[2],stats[3],stats[4],stats[5])
+}
